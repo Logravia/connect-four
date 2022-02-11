5f3d5956df6e1b2dd97cfc512c578fb4bd669324
Jānis Alunāns <j.alunans@gmail.com>
2022-02-11 21:54:12 +0200

    Add choose_token

--- lib/player.rb
+++ lib/player.rb
@@ -7,9 +7,14 @@ require 'pry-byebug'
 # Allows player to interact with the game by handling column choice of the player
 # And letting the game handle any special input players might've put in
 # E.g. q for quit, h for help
+
 class Player
-  LEGIT_CHOICE = /^[1-8]$/.freeze
-  private_constant :LEGIT_CHOICE
+  LEGIT_COLUMNS = /^[1-8]$/.freeze
+  LEGIT_TOKENS= /^.$/.freeze
+  SPECIAL_INPUT = /^[qh]!$/.freeze
+
+  private_constant :LEGIT_COLUMNS , :LEGIT_TOKENS, :SPECIAL_INPUT
+  attr_reader :token
 
   def initialize(game)
     @game = game
@@ -18,21 +23,40 @@ class Player
   end
 
   def choose_column
-    loop do
-      input = gets.chomp
-      break if @game.special_input?(input)
+    return get_value(LEGIT_COLUMNS, 'Wrong column').to_i - 1
+  end
 
-      column = sanitize_column_choice(input)
-      return column unless column.nil?
-
-      puts 'MSG'
-    end
+  def choose_token
+    @token = get_value(LEGIT_TOKENS, 'Wrong token')
   end
 
   private
 
-  def sanitize_column_choice(choice)
-    choice = choice.match(LEGIT_CHOICE)
-    choice.nil? ? nil : choice.to_s.to_i
+  def get_value(valid_value, error_msg)
+    value = nil
+    loop do
+      input = gets.chomp
+
+      if special_input?(input)
+        @game.handle_special_input(input)
+        return
+      end
+
+      value = sanitize_input(input, valid_value)
+      break unless value.nil?
+
+      puts error_msg
+    end
+    value
   end
+
+  def sanitize_input(choice, pattern)
+    choice = choice.match(pattern)
+    choice.nil? ? nil : choice.to_s
+  end
+
+  def special_input?(input)
+    input.match(SPECIAL_INPUT) ? true : false
+  end
+
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -12,13 +12,13 @@ describe Player do
 
     context 'handles valid and invalid input' do
       before do
-        allow(game_dummy).to receive(:special_input?).and_return(nil)
+        allow(game_dummy).to receive(:handle_special_input).and_return(nil)
       end
 
-      it 'returns the chosen column as single positive digit' do
+      it 'returns the chosen column as single positive digit reduced by one' do
         allow(player).to receive(:gets).and_return("1\n")
         chosen_column = 1
-        expect(player.choose_column).to eq(chosen_column)
+        expect(player.choose_column).to eq(chosen_column-1)
       end
 
       it 'puts message when bad input, stops get loop after good message' do
@@ -42,8 +42,58 @@ describe Player do
       it 'ends its loop after special input and lets game handle it' do
         special_input = "h\n"
         allow(player).to receive(:gets).and_return(special_input)
-        allow(game_dummy).to receive(:special_input?).with(special_input).and_return(true)
+        allow(game_dummy).to receive(:handle_special_input).with(special_input).and_return(true)
         expect(player).not_to receive(:puts)
+      end
+    end
+
+    describe '#choose_token' do
+      let(:game_dummy) { instance_double(Game) }
+      subject(:player) { described_class.new(game_dummy) }
+
+      context "#handles valid input" do
+        it "sets token" do
+          token = 'X'
+          allow(player).to receive(:gets).and_return(token)
+          player.choose_token
+          expect(player.token).to eq(token)
+        end
+      end
+      context "#rejects invalid input with error message" do
+        it "warns three times of invalid input" do
+          valid_input = 'X'
+          allow(player).to receive(:gets).and_return('asdas', 'as', 'vbsdv', valid_input, 'asb')
+          expect(player).to receive(:puts).exactly(3).times
+          player.choose_token
+        end
+        context "special input" do
+          before do
+            allow(game_dummy).to receive(:handle_special_input)
+          end
+          it "does not save invalid input as token" do
+            invalid_input = 'QED'
+            special_input = 'h!'
+
+            allow(player).to receive(:gets).and_return(invalid_input, special_input)
+
+            player.choose_token
+            expect(player.token).to be_nil
+          end
+
+          it "asks game to handle special input" do
+            special_input = 'h!'
+            allow(player).to receive(:gets).and_return(special_input)
+            expect(game_dummy).to receive(:handle_special_input).once
+            player.choose_token
+          end
+
+          it "ends input loop on special input" do
+            special_input = 'h!'
+            allow(player).to receive(:gets).and_return(special_input)
+            expect(player.choose_token).to be_nil
+          end
+        end
+
       end
     end
   end
