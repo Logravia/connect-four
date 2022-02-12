5f15397f07197bba697e4a189957e6b4977ec220
Jānis Alunāns <j.alunans@gmail.com>
2022-02-12 17:27:52 +0200

    Add ability to set tokens at game start

--- lib/game.rb
+++ lib/game.rb
@@ -8,7 +8,21 @@ class Game
     @round = 0
   end
 
-  def special_input?(input); end
+  def set_player_tokens
+
+    @players.each_with_index do |player, num|
+      @display.game(@board.token_grid, "Player #{num+1}, #{Msg.choose_token} ")
+      player.choose_token
+      @display.game(@state, "#{Msg.good_token}: #{player.token}")
+      #sleep(2)
+    end
+
+    while @players[1].token == @players[0].token
+      @display.game(@state, "#{Msg.same_token}: #{@players[1].token}")
+      @players[1].choose_token
+    end
+  end
+
 
 
 end
